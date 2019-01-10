95acacd2fb4b85c994d02125638f903bd9b91322
PProvost <peter@provost.org>
2019-01-10 11:59:36 -0700

    More tweaks to secrets backup

--- secrets_backup.sh
+++ secrets_backup.sh
@@ -2,21 +2,22 @@
 
 pushd /config
 
-scp .ios.conf peter@home-nas.lan:~/home-assistant-secrets
-scp .ring_cache.pickle peter@home-nas.lan:~/home-assistant-secrets
-scp ecobee.conf peter@home-nas.lan:~/home-assistant-secrets
 # scp home-assistant_v2.db peter@home-nas.lan:~/home-assistant-secrets
-scp home-assistant.log peter@home-nas.lan:~/home-assistant-secrets
-scp ip_bans.yaml peter@home-nas.lan:~/home-assistant-secrets
-scp known_devices.yaml peter@home-nas.lan:~/home-assistant-secrets
-scp life360.conf peter@home-nas.lan:~/home-assistant-secrets
-scp OZW_Log.txt peter@home-nas.lan:~/home-assistant-secrets
-scp plex.conf peter@home-nas.lan:~/home-assistant-secrets
 # scp pyozw.sqlite peter@home-nas.lan:~/home-assistant-secrets
-scp sabnzbd.conf peter@home-nas.lan:~/home-assistant-secrets
-scp secrets.yaml peter@home-nas.lan:~/home-assistant-secrets
-scp ui-lovelace.yaml peter@home-nas.lan:~/home-assistant-secrets
-scp zwcfg_0xc13cc76f.xml peter@home-nas.lan:~/home-assistant-secrets
-scp zwscene.xml peter@home-nas.lan:~/home-assistant-secrets
+
+scp -i /root/.ssh/id_rsa .ios.conf peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa .ring_cache.pickle peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa ecobee.conf peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa home-assistant.log peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa ip_bans.yaml peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa known_devices.yaml peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa life360.conf peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa OZW_Log.txt peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa plex.conf peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa sabnzbd.conf peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa secrets.yaml peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa ui-lovelace.yaml peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa zwcfg_0xc13cc76f.xml peter@home-nas.lan:~/home-assistant-secrets
+scp -i /root/.ssh/id_rsa zwscene.xml peter@home-nas.lan:~/home-assistant-secrets
 
 popd
