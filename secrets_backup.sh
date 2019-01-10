3e7478b603f62314c2b9188ed4c62382aad527de
PProvost <peter@provost.org>
2019-01-10 11:51:07 -0700

    More secrets back up

--- secrets_backup.sh
+++ secrets_backup.sh
@@ -5,14 +5,14 @@ pushd /config
 scp .ios.conf peter@home-nas.lan:~/home-assistant-secrets
 scp .ring_cache.pickle peter@home-nas.lan:~/home-assistant-secrets
 scp ecobee.conf peter@home-nas.lan:~/home-assistant-secrets
-scp home-assistant_v2.db peter@home-nas.lan:~/home-assistant-secrets
+# scp home-assistant_v2.db peter@home-nas.lan:~/home-assistant-secrets
 scp home-assistant.log peter@home-nas.lan:~/home-assistant-secrets
 scp ip_bans.yaml peter@home-nas.lan:~/home-assistant-secrets
 scp known_devices.yaml peter@home-nas.lan:~/home-assistant-secrets
 scp life360.conf peter@home-nas.lan:~/home-assistant-secrets
 scp OZW_Log.txt peter@home-nas.lan:~/home-assistant-secrets
 scp plex.conf peter@home-nas.lan:~/home-assistant-secrets
-scp pyozw.sqlite peter@home-nas.lan:~/home-assistant-secrets
+# scp pyozw.sqlite peter@home-nas.lan:~/home-assistant-secrets
 scp sabnzbd.conf peter@home-nas.lan:~/home-assistant-secrets
 scp secrets.yaml peter@home-nas.lan:~/home-assistant-secrets
 scp ui-lovelace.yaml peter@home-nas.lan:~/home-assistant-secrets
