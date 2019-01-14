c9518b672417bd1e8c15b7af820a1a594c093eef
PProvost <peter@provost.org>
2019-01-14 13:30:17 -0700

    AppDaemon tweaks

--- appdaemon/apps/motion_lights.py
+++ appdaemon/apps/motion_lights.py
@@ -1,24 +1,42 @@
 import appdaemon.plugins.hass.hassapi as hass
 
 class SimpleMotionLight(hass.Hass):
+  """
+  Monitors a list of binary_sensors; when any turn on, turn on the lights; after
+  a configurable delay, turn them off. Retriggering during the delay period will
+  extend the delay.
+
+  Arguments:
+    - binary_sensors    - (list) which binary_sensors to monitor for state change
+    - lights            - (list) which lights to turn on/off
+    - turn_off_delay    - (int) how many seconds before turning off
+  """
+
   def initialize(self):
     self.timer_handle = None
-    if "light" not in self.args:
-      self.log("argument 'light' not provided")
-      return
 
-    if "binary_sensor" in self.args:
-      self.log("watching {}".format(self.args['binary_sensor']))
-      self.listen_state(self.state_change_handler, self.args["binary_sensor"], new="on")
+    if "binary_sensors" in self.args:
+      self.log("watching {}".format(self.args['binary_sensors']))
+      for entity in self.args['binary_sensors']:
+        self.listen_state(self.state_change_handler, entity, new="on")
     else:
       self.log("argument 'binary_sensor' not provided", level = "ERROR")
-  
-  def state_change_handler(self, entity, attribute, old, new, kwargs):
-    self.log("{} state changed (old: {}, new: {})".format(entity, old, new))
-    self.turn_on(self.args['light'])
+      return
 
-    if "off_delay" in self.args:
-      delay = self.args["off_delay"]
+    if "lights" in self.args:
+      self.log("will turn on/off these lights: {}".format(self.args['lights']))
+    else:
+      self.log("argument 'lights' not provided")
+      return
+
+  def state_change_handler(self, entity, attribute, old, new, kwargs):
+
+    self.log("turning on {}".format(self.args['lights']))
+    for entity in self.args['lights']:
+      self.turn_on(entity)
+
+    if "turn_off_delay" in self.args:
+      delay = self.args["turn_off_delay"]
     else:
       delay = 60
     
@@ -28,5 +46,6 @@ class SimpleMotionLight(hass.Hass):
     self.timer_handle = self.run_in(self.turn_off_callback, delay)
 
   def turn_off_callback(self, kwargs):
-    self.log("turning off")
-    self.turn_off(self.args['light'])
+    self.log("turning off {}".format(self.args['lights']))
+    for entity in self.args['lights']:
+      self.turn_on(entity)
